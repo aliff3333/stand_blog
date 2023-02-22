93797f8dd44a1c537954208562ed3864e0d15d34
AliSayyadi <aliff.3333@gmail.com>
2023-02-23 01:10:31 +0330

    add login and register views

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login)
+    path('login', views.login_view),
+    path('logout', views.logout_view),
+    path('register', views.register_view),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
-def login(request):
-    return render(request, 'account/login.html')
+def login_view(request):
+    check_user = request.user.is_authenticated
+    if check_user:
+        return redirect('/')
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
+    return render(request, 'account/login.html', context={})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+
+def register_view(request):
+    context ={'errors': []}
+
+    check_user = request.user.is_authenticated
+    if check_user:
+        return redirect('/')
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm-password')
+        if User.objects.get(username=username):
+            context['errors'].append('This username exists.')
+            return render(request, 'account/register.html', context=context)
+        if password != confirm_password:
+            context['errors'].append('Passwords do not match.')
+            return render(request, 'account/register.html', context=context)
+        user = User.objects.create(username=username, email=email, password=password)
+        login(request, user)
+        return redirect('/')
+
+    return render(request, 'account/register.html', context={})
