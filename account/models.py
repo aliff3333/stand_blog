ff1add05691f53c4b4ce559382d2ce3fd4ba7add
AliSayyadi <aliff.3333@gmail.com>
2023-02-23 19:20:10 +0330

    add profile model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    fathers_name = models.CharField(max_length=50)
+    melicode = models.CharField(max_length=10)
+    image = models.ImageField(upload_to='images/profiles', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
