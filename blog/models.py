f376baa0a00a6b399c0faa70e90dd453cf898d8a
AliSayyadi <aliff.3333@gmail.com>
2023-02-23 17:49:26 +0330

    add author and category models

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+    title = models.CharField(max_length=100, )
     body = models.TextField()
     image = models.ImageField(upload_to='images/articles')
     created = models.DateTimeField(auto_now_add=True)
