db86abf64dfc7ba43fcf0586de360a70d1a6119f
AliSayyadi <aliff.3333@gmail.com>
2023-02-24 17:47:56 +0330

    add article details

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 
-# Create your views here.
+
+def article_details(request, pk):
+    article = get_object_or_404(Article, id=pk)
+    return render(request, 'blog/article_details.html', context={'article': article})
--- stand_blog/urls.py
+++ stand_blog/urls.py
@@ -6,5 +6,6 @@ from . import settings
 urlpatterns = [
     path('', include('home.urls')),
     path('', include('account.urls')),
+    path('articles/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
